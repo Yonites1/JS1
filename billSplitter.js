9077613f7ab521b4f0646d54ed1aee9424724fbd
Yonites1 <yonites49@gmail.com>
2024-04-28 20:52:53 -0700

    add new changes

---
+++ billSplitter.js
@@ -0,0 +1,14 @@
+let totalBillAmount = 250;
+let numberOfPeople = 6;
+let tipPercentages = 20;
+let taxPercentages = 10;
+
+let tipAmount = (totalBillAmout * tipPercentages)/100;
+let taxAmount = (totalBillAmount * taxPercentages)/100;
+let totalAmount = totalBillAmount + tipAmount + taxAmount;
+let individualBill = totalAmount / numberOfPeople;
+
+console.log (`tipAmount: ${{tipAmount}}`);
+console.log (`taxAmount: ${{taxAmount}}`);
+console.log (`totalAmount: ${totalAmount}`);
+console.log (`individual Bill: ${individualBill}`);
